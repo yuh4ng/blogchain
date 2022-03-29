4eec6bece4032020d6325e6bbe093598fd1e0cf9
Blazeboard <blazeboard@outlook.com>
2022-03-30 01:47:43 +0800

    none

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -331,7 +331,7 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
+		[pallet_blogchain, Blogchain]
 	);
 }
 
